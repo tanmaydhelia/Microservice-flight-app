53035ff581ebac998ee8331f4afe53b6068bf0f2
tanmaydhelia <tanmaydhelia03@gmail.com>
2025-12-15 13:11:37 +0530

    Revert "Update: Added Dockerfiles in respective projects"
    This reverts commit 83b7e0662e4442d2afb30d00e9d3cc3e2f3ceba2.

--- flightapp-api-gateway/src/main/java/com/flightapp/apigateway/filter/AuthenticationFilter.java
+++ flightapp-api-gateway/src/main/java/com/flightapp/apigateway/filter/AuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.flightapp.apigateway.filter;
 
+import java.io.ObjectInputFilter.Config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
